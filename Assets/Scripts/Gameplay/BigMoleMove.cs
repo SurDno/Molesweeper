71bbe6639d51a99e57c74d55ffd01471693f5c3a
SurDno <surdno@yandex.ru>
2023-02-05 15:55:08 +0000

    last

--- Assets/Scripts/Gameplay/BigMoleMove.cs
+++ Assets/Scripts/Gameplay/BigMoleMove.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class BigMoleMove : MonoBehaviour
 {
+	public GameObject SpaceBar;
+	
+    float timer =0;
+    float lerpPosition, moveSpeed = 0.5f;
+    Vector3 currentPosition, targetPosition;
+    bool moving;
     // Start is called before the first frame update
     void Start()
     {
-        this.gameObject.transform
+        UpdateCurrentPosition();
+        targetPosition = new Vector3(22,4,0);
+        moving = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if(moving && timer > 2)
+        {
+            Move();
+        }
+    }
+
+    void UpdateCurrentPosition()
+    {
+        int currentX = (int)transform.position.x;
+        int currentY = (int)transform.position.y;
+        currentPosition = new Vector3(currentX, currentY, 0);
+    }
+
+    void Move()
+    {
+        lerpPosition += Time.deltaTime * moveSpeed;
+        transform.position = Vector3.Lerp(currentPosition, targetPosition, lerpPosition);
+
+        if(lerpPosition >= 1)
+        {
+            moving = false;
+            lerpPosition = 0;
+            UpdateCurrentPosition();
+			SpaceBar.SetActive(true);
+        }
+		
     }
 }
